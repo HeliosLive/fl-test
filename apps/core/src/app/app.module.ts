4352d88bf6a1bf58c1a03e3f0f6eb7d4e5e18ad8
Ahmet Aydin <ahmetttaydin@yandex.com>
2021-05-08 13:05:12 +0200

    button component added

--- apps/core/src/app/app.module.ts
+++ apps/core/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from '@fl-test/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/fl-core/src/index.ts
+++ libs/fl-core/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/fl-core.module';
+export * from './lib/button/button.module';
